c37245f743d1a3494ca346b8df4398a0dfb37e4d
SqGoliaf <ivanov@v-1336.ru>
2023-03-29 15:54:30 +0300

    .

--- algoritms1/algoritms2.py
+++ algoritms1/algoritms2.py
@@ -1,9 +1,15 @@
 def find_max(seq):
-    ans = seq[0]
+    # ans = seq[0]
+    # for i in range(1,len(seq)):
+    #     if seq[i] > ans:
+    #         ans = seq[i]
+    # return ans
+
+    ans = 0
     for i in range(1, len(seq)):
-        if seq[i] > ans:
-            ans = seq[i]
-    return ans
+        if seq[i] > seq[ans]:
+            ans = i
+    return seq[ans]
 
 if __name__ == '__main__':
     print(find_max([1,2,3,4,5]))
